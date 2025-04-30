b306e6008a6835bf0020ed0c02879025fb3956cb
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-04-30 21:39:39 +0400

    navbar code add

---
+++ app.js
@@ -0,0 +1,23 @@
+// var menuBar = document.querySelector('.desktopNav');
+// window.addEventListener('scroll', scrollFunction);
+// function scrollFunction() {
+//     if (window.scrollY > 150) {
+//         menuBar.classList.add('active')
+//     } else {
+//         menuBar.classList.remove('active')
+//     }
+// }
+
+// const Navbar = document.querySelector(".desktopNav");
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 300) {
+//     console.log(salam)
+//     if (Navbar) {
+//       Navbar.classList.add("active");
+//     }
+//   } else {
+//     if (Navbar) {
+//       Navbar.classList.remove("active");
+//     }
+//   }
+// });
